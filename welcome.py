6b906b9ee1e9d68aa75dbb9b99b113816a60027c
Anna Karutina <anna.karutina@khk.ee>
2026-01-08 11:08:56 +0200

    Ask the user to participate in a simple survey to determine their experience (on a scale of 1 to 5)

--- welcome.py
+++ welcome.py
@@ -1,5 +1,6 @@
 from validate_age import *
 from get_name import *
+from survey import *
 
 age = int(input("Enter your age: "))
 validate_age(age)
@@ -11,3 +12,10 @@ print("\n")
 
 user_name = get_name()
 print(f"Hello {user_name}!\n")
+
+survey_flag = input("Would you like to participate in our survey? (Y/N)")
+
+if survey_flag == 'Y':
+    survey()
+    
+print("\nThank You! Please visit again.\n\n")
